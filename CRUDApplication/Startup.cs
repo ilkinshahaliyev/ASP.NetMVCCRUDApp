fe5cab80778269012f7708eb728693d9c1aa4a55
Ilkin Shahaliyev <shahaliyev2021@gmail.com>
2022-09-11 12:22:07 +0400

    Project updated

--- CRUDApplication/Startup.cs
+++ CRUDApplication/Startup.cs
@@ -22,7 +22,6 @@ namespace CRUDApplication
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -30,7 +29,6 @@ namespace CRUDApplication
                 options.UseSqlServer(Configuration["ConnectionString"]));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,7 +38,6 @@ namespace CRUDApplication
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
